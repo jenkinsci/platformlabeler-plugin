b04de4acd133f4b69d45b2cb3d73fd502ee524a7
Nicolas De Loof <nicolas.deloof@gmail.com>
2012-02-10 09:37:26 +0100

    [FIXED JENKINS-10383] svn patch applied thanks to Ammon Riley

--- src/main/java/org/jvnet/hudson/plugins/platformlabeler/PlatformDetailsTask.java
+++ src/main/java/org/jvnet/hudson/plugins/platformlabeler/PlatformDetailsTask.java
@@ -29,19 +29,35 @@ import net.robertcollins.lsb.Release;
 
 import hudson.remoting.Callable;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.InterruptedException;
 import java.util.HashSet;
 
 class PlatformDetailsTask implements Callable<HashSet<String>, IOException> {
 
     /** Performs computation and returns the result, or throws some exception. */
     public HashSet<String> call() throws IOException {
-        final String arch = System.getProperty("os.arch");
+        String arch = System.getProperty("os.arch").toLowerCase();
         String name = System.getProperty("os.name").toLowerCase();
         String version = System.getProperty("os.version");
         if (name.equals("solaris") || name.equals("SunOS")) {
             name = "solaris";
         } else if (name.startsWith("windows")) {
             name = "windows";
+            /** os.arch is really the JRE bitness, not the OS bitness. */
+            if ("x86".equals(arch)) {
+                final String env1 =
+                    System.getenv("PROCESSOR_ARCHITECTURE").toLowerCase();
+                String env2 = null;
+                try {
+                    env2 = System.getenv("PROCESSOR_ARCHITEW6432").toLowerCase();
+                } catch (Exception e) {}
+
+                if ("amd64".equals(env1) || "amd64".equals(env2)) {
+                    arch = "amd64";
+                }
+            }
             if (name.startsWith("windows 9")) {
                 if (version.startsWith("4.0")) {
                     version = "95";
@@ -59,7 +75,25 @@ class PlatformDetailsTask implements Callable<HashSet<String>, IOException> {
                 } else if (version.startsWith("5.1")) {
                     version = "xp";
                 } else if (version.startsWith("5.2")) {
-                    version = "2003";
+                    if ("amd64".equals(arch)) {
+                        // The 64-bit version of xp is based on 2003
+                        version = "2003+xp";
+                    } else {
+                        version = "2003";
+                    }
+                } else if (version.startsWith("6.0.6000")) {
+                    version = "vista";
+                } else if (version.startsWith("6.0")) {
+                    // Server 2008 is based on 6.0.6001
+                    version = "vista+2008";
+                } else if (version.startsWith("6.1")) {
+                    if ("x86".equals(arch)) {
+                        // 2008 R2 is 64-bit only.
+                        version = "7";
+                    } else {
+                        // TODO distinguish windows 7amd64 from 2008R2?
+                        version = "7+2008r2";
+                    }
                 }
             }
         } else if (name.startsWith("linux")) {
@@ -71,6 +105,23 @@ class PlatformDetailsTask implements Callable<HashSet<String>, IOException> {
             version = release.release();
             if (null == version)
                 version = unknown_string;
+
+            if ("x86".equals(arch)) {
+                Process p = Runtime.getRuntime().exec("/bin/uname -m");
+                try {
+                    p.waitFor();
+                    BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                    String line = b.readLine();
+                    if (line != null) {
+                        if ("x86_64".equals(line)) {
+                            arch = "amd64";
+                        } else {
+                            arch = line;
+                        }
+                    }
+                }
+                catch (InterruptedException e) {}
+            }
         } else if (name.startsWith("mac")) {
             name = "mac";
         } else {
