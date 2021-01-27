84a46b8525020ff14e2883f7c847c9bbbdf12f62
Mark Waite <mark.earl.waite@gmail.com>
2021-01-27 13:53:05 -0700

    Use dependabot for all test Dockerfiles
    Also includes a script to update the dependabot file when things change.

---
+++ src/test/resources/org/jvnet/hudson/plugins/platformlabeler/update_dependabot.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Update dependabot definition for current files
+
+if [ ! -f .github/dependabot.yml ]; then
+  cd src/test/resources/org/jvnet/hudson/plugins/platformlabeler || exit 1
+fi
+
+cat > .github/dependabot.yml <<END-OF-DEPENDABOT-YML
+# Per https://docs.github.com/en/github/administering-a-repository/configuration-options-for-dependency-updates
+version: 2
+updates:
+- package-ecosystem: maven
+  directory: "/"
+  schedule:
+    interval: weekly
+  open-pull-requests-limit: 10
+  target-branch: master
+  reviewers:
+  - MarkEWaite
+  labels:
+  - skip-changelog
+  ignore:
+  - dependency-name: org.slf4j*
+    versions:
+    - ">= 0"
+END-OF-DEPENDABOT-YML
+
+for file in $(git ls-files -- *Dockerfile | sort); do
+  dir=$(dirname $file)
+  cat >> .github/dependabot.yml <<END-OF-DEPENDABOT-YML
+
+- package-ecosystem: docker
+  directory: "${dir}"
+  schedule:
+    interval: weekly
+  open-pull-requests-limit: 2
+  target-branch: master
+  reviewers:
+  - MarkEWaite
+  labels:
+  - skip-changelog
+END-OF-DEPENDABOT-YML
+done
