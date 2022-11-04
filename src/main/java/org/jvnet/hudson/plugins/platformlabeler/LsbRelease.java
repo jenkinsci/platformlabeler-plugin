42c6f14076f5d9abbbfe001e8db65db6c0ff4627
Mark Waite <mark.earl.waite@gmail.com>
2022-11-04 05:15:01 -0600

    [JENKINS-69971] Assign Debian testing label correctly
    Debian testing has periods during its lifecycle when the lsb_release
    command cannot distinguish between testing and unstable.  That is an
    accepted condition for the Debian developers.
    
    See https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=845651
    
    When in that state, the apt-cache command can be used to determine the
    Debian release that is running.
    
    The base-files package is used to query the apt-cache policy because it
    is required for all Debian installations and because it is the package
    that provides the /etc/os-release file that is used elsewhere to determine
    version information in case the lsb_release command is not available.

--- src/main/java/org/jvnet/hudson/plugins/platformlabeler/LsbRelease.java
+++ src/main/java/org/jvnet/hudson/plugins/platformlabeler/LsbRelease.java
@@ -32,10 +32,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /** Linux standard base release class. Provides distributor ID and release. */
 public class LsbRelease implements PlatformDetailsRelease {
@@ -55,7 +58,12 @@ public class LsbRelease implements PlatformDetailsRelease {
         }
         this.distributorId =
                 newProps.getOrDefault("Distributor ID", PlatformDetailsTask.UNKNOWN_VALUE_STRING);
-        this.release = newProps.getOrDefault("Release", PlatformDetailsTask.UNKNOWN_VALUE_STRING);
+        String guessedRelease =
+                newProps.getOrDefault("Release", PlatformDetailsTask.UNKNOWN_VALUE_STRING);
+        if (guessedRelease.equals("n/a")) {
+            guessedRelease = guessDebianRelease(guessedRelease);
+        }
+        this.release = guessedRelease;
     }
 
     /** Assign distributor ID and release. Package protected for tests. */
@@ -72,7 +80,12 @@ public class LsbRelease implements PlatformDetailsRelease {
         }
         this.distributorId =
                 newProps.getOrDefault("Distributor ID", PlatformDetailsTask.UNKNOWN_VALUE_STRING);
-        this.release = newProps.getOrDefault("Release", PlatformDetailsTask.UNKNOWN_VALUE_STRING);
+        String guessedRelease =
+                newProps.getOrDefault("Release", PlatformDetailsTask.UNKNOWN_VALUE_STRING);
+        if (guessedRelease.equals("n/a")) {
+            guessedRelease = guessDebianReleaseFromFile(lsbReleaseFile);
+        }
+        this.release = guessedRelease;
     }
 
     private void readLsbReleaseOutput(InputStream inputStream, Map<String, String> newProps)
@@ -84,6 +97,70 @@ public class LsbRelease implements PlatformDetailsRelease {
                     .map(line -> line.split(":", 2))
                     .forEach(parts -> newProps.put(parts[0], parts[1].trim()));
         }
+    }
+
+    /*
+     * In the test environment, the intended Debian version is encoded
+     * in the path to the file.  If a file was provided with test
+     * data, then the path of that file resolves the question whether
+     * the ambiguous Debian version is testing or unstable.
+     */
+    private String guessDebianReleaseFromFile(@NonNull File testData) {
+        String unstableDir = File.separator + "unstable" + File.separator;
+        return testData.getPath().contains(unstableDir) ? "unstable" : "testing";
+    }
+
+    /*
+     * Debian testing has periods during its lifecycle when the
+     * lsb_release command cannot distinguish between testing and
+     * unstable.  That is an accepted condition for the Debian
+     * developers.
+     *
+     * See https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=845651
+     * for more details.
+     *
+     * When in that state, the apt-cache command can be used to
+     * determine the Debian release that is running.
+     *
+     * The base-files package is used to query the apt-cache policy
+     * because it is required for all Debian installations and because
+     * it is the package that provides the /etc/os-release file that
+     * is used elsewhere to determine version information in case the
+     * lsb_release command is not available.
+     */
+    private String guessDebianRelease(@NonNull String defaultValue) {
+        String value = defaultValue;
+        try {
+            Process process = new ProcessBuilder("apt-cache", "policy", "base-files").start();
+            value = readReleaseFromAptCachePolicyOutput(process.getInputStream());
+        } catch (IOException e) {
+            LOGGER.log(Level.FINEST, "apt-cache execution failed", e);
+        }
+        return value;
+    }
+
+    /* Identifying strings in apt-cache policy output that identify
+     * the unstable distribution
+     */
+    private List<String> unstableIdentifiers = Arrays.asList("sid", "unstable");
+
+    /*
+     * If apt-cache output contains one of the unstableIdentifiers,
+     * report it as unstable.  Otherwise report it as testing.
+     */
+    private String readReleaseFromAptCachePolicyOutput(InputStream inputStream) throws IOException {
+        String distribution = "testing";
+        try (BufferedReader reader =
+                new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            List<String> results =
+                    reader.lines()
+                            .filter(line -> unstableIdentifiers.stream().anyMatch(line::contains))
+                            .collect(Collectors.toList());
+            if (!results.isEmpty()) {
+                distribution = "unstable";
+            }
+        }
+        return distribution;
     }
 
     /**
