098a9523fa7b6336db3a4c6e3dfe70a09dee36db
Mark Waite <mark.earl.waite@gmail.com>
2022-11-25 19:44:57 -0700

    Add redhat-release data generator and files

--- src/test/resources/org/jvnet/hudson/plugins/platformlabeler/generate_test_data.sh
+++ src/test/resources/org/jvnet/hudson/plugins/platformlabeler/generate_test_data.sh
@@ -14,6 +14,8 @@ fi
 
 args=$@
 
+# Generate os-release files
+# Generate redhat-release files for all systems that include the redhat-release file
 for Dockerfile in $(find * -type f -name Dockerfile ! -path '*7.9.2009*' -print); do
         name_version=$(dirname $Dockerfile)
         name=$(dirname $name_version)
@@ -21,6 +23,13 @@ for Dockerfile in $(find * -type f -name Dockerfile ! -path '*7.9.2009*' -print)
         echo "Processing Dockerfile $Dockerfile for name $name and version $version"
         (cd $name_version && docker build --pull -t platformlabeler/$name:$version .)
         docker run --rm -t platformlabeler/$name:$version cat /etc/os-release | tr -d '\015' > $name_version/os-release
+        docker run --rm -t platformlabeler/$name:$version cat /etc/redhat-release | tr -d '\015' > $name_version/redhat-release
+        # Remove redhat-release file if it does not exist in the container image
+        grep -q -i cat:.*no.such.file $name_version/redhat-release && rm -rf $name_version/redhat-release
+        # Remove redhat-release file for Oracle Linux, it contains the wrong vendor name
+        if [ "$name" = "oraclelinux" ]; then
+                rm -rf $name_version/redhat-release
+        fi
 done
 
 exit 0
