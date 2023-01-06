53f2024bc98bb9596661e1f7b1d70743c2770376
Mark Waite <mark.earl.waite@gmail.com>
2023-01-06 13:54:32 -0700

    Automate formatting (#888)
    * Format code and pom with spotless
    
    * Update CONTRIBUTING for new formatting

--- src/test/java/org/jvnet/hudson/plugins/platformlabeler/LsbReleaseFakeFileTest.java
+++ src/test/java/org/jvnet/hudson/plugins/platformlabeler/LsbReleaseFakeFileTest.java
@@ -8,14 +8,12 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 public class LsbReleaseFakeFileTest {
-    @TempDir
-    File dataDir;
+    @TempDir File dataDir;
 
     private String fakeDistributorId = "megasupercorp";
     private String fakeRelease = "1.2.3";
@@ -23,9 +21,11 @@ public class LsbReleaseFakeFileTest {
 
     @BeforeEach
     void initialize() throws Exception {
-        List<String> data = Arrays.asList("Unexpected line that should be ignored",
-                                          "Distributor ID: " + fakeDistributorId,
-                                          "Release: " + fakeRelease);
+        List<String> data =
+                Arrays.asList(
+                        "Unexpected line that should be ignored",
+                        "Distributor ID: " + fakeDistributorId,
+                        "Release: " + fakeRelease);
         File dataFile = new File(dataDir, "lsb_release_fake");
         Files.write(dataFile.toPath(), data, StandardCharsets.UTF_8);
         fakeLsbRelease = new LsbRelease(dataFile);
